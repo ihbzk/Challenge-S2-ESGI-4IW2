f4e1bf4eb1439eb0c78e458864010f3d741180ba
Théo <davigny.theo@gmail.com>
2024-07-14 18:34:07 +0200

    fix

--- back/src/controllers/user.controller.js
+++ back/src/controllers/user.controller.js
@@ -372,7 +372,7 @@ exports.updateUser = async (req, res) => {
         const user = await User.findByPk(id);
 
         if (!user) {
-            return res.status(404).json({ message: 'User not found' });
+            return res.status(404);
         }
 
         const { firstname, lastname, email, password, role } = req.body;
@@ -449,7 +449,7 @@ exports.isAdministrator = async (req, res, next) => {
         const user = await User.findByPk(req.user.id);
 
         if (!user) {
-            return res.sendStatus(404); // User not found
+            return res.sendStatus(404);
         }
 
         if (user.role !== 'ROLE_ADMIN') {
