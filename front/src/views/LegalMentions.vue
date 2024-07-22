7f5b99bf0ea72a44ac9eb0e741ceb4706611f971
Victor De Domenico <victordedomenico.du60@gmail.com>
2024-07-22 15:08:08 +0200

    chore: Update files to improve readability and formatting

--- front/src/views/LegalMentions.vue
+++ front/src/views/LegalMentions.vue
@@ -1,49 +1,56 @@
 <template>
-  <div class="legal-mentions bg-gray-100 rounded-lg max-w-3xl mx-auto p-5">
-    <h1 class="text-gray-900 text-2xl mb-4">Mentions Légales</h1>
-    <p class="text-gray-600 mb-4">Bienvenue sur notre site InformaCart.</p>
-    <p class="text-gray-600 mb-4">
-      Conformément aux dispositions des Articles 6-III et 19 de la Loi n°2004-575
-      du 21 juin 2004 pour la Confiance dans l’économie numérique, dite L.C.E.N.,
-      nous informons les utilisateurs et visiteurs du site informacart.com les informations suivantes :
-    </p>
-    <h2 class="text-indigo-600 text-xl mt-6 mb-4">Éditeur du site</h2>
-    <p class="text-gray-600 mb-4">
-      Nom de la Société : InformaCart<br>
-      Forme juridique : Société par Actions Simplifiée (SAS)<br>
-      Capital social : 100 000 €<br>
-      Adresse : 123 Rue de la Technologie, 75001 Paris<br>
-      Téléphone : 01 23 45 67 89<br>
-      Email : contact@informacart.com<br>
-      Numéro SIRET : 123 456 789 00010<br>
-      Numéro TVA intracommunautaire : FR 12 345678900
-    </p>
-    <h2 class="text-indigo-600 text-xl mt-6 mb-4">Directeur de la publication</h2>
-    <p class="text-gray-600 mb-4">
-      Le Directeur de la publication est Monsieur Jean Dupont, en qualité de Président de la société InformaCart.
-    </p>
-    <h2 class="text-indigo-600 text-xl mt-6 mb-4">Hébergement</h2>
-    <p class="text-gray-600 mb-4">
-      Le site est hébergé par :<br>
-      Nom de l'hébergeur : HostingProvider<br>
-      Forme juridique : Société à Responsabilité Limitée (SARL)<br>
-      Adresse : 456 Avenue de l'Internet, 75002 Paris<br>
-      Téléphone : 01 98 76 54 32<br>
-      Email : support@hostingprovider.com
-    </p>
-    <h2 class="text-indigo-600 text-xl mt-6 mb-4">Propriété intellectuelle</h2>
-    <p class="text-gray-600 mb-4">
-      Tous les éléments graphiques, la structure et, plus généralement, le contenu du site InformaCart sont protégés par le droit d'auteur, le droit des marques et le droit des dessins et modèles.
-    </p>
-    <h2 class="text-indigo-600 text-xl mt-6 mb-4">Données personnelles</h2>
-    <p class="text-gray-600 mb-4">
-      Conformément à la loi Informatique et Libertés du 6 janvier 1978, vous disposez d'un droit d'accès, de rectification, de modification et de suppression des données qui vous concernent. Vous pouvez exercer ce droit en envoyant un courrier à l'adresse suivante : InformaCart, 123 Rue de la Technologie, 75001 Paris.
-    </p>
-    <h2 class="text-indigo-600 text-xl mt-6 mb-4">Conditions d'utilisation</h2>
-    <p class="text-gray-600 mb-4">
-      L'utilisation du site InformaCart implique l'acceptation pleine et entière des conditions générales d'utilisation décrites ci-après. Ces conditions d'utilisation sont susceptibles d'être modifiées ou complétées à tout moment, les utilisateurs du site InformaCart sont donc invités à les consulter de manière régulière.
-    </p>
-  </div>
+  <main>
+    <div class="legal-mentions bg-gray-100 rounded-lg max-w-3xl mx-auto p-5">
+      <h1 class="text-gray-900 text-2xl mb-4">Mentions Légales</h1>
+      <p class="text-gray-600 mb-4">Bienvenue sur notre site InformaCart.</p>
+      <p class="text-gray-600 mb-4">
+        Conformément aux dispositions des Articles 6-III et 19 de la Loi n°2004-575
+        du 21 juin 2004 pour la Confiance dans l’économie numérique, dite L.C.E.N.,
+        nous informons les utilisateurs et visiteurs du site informacart.com les informations suivantes :
+      </p>
+      <h2 class="text-indigo-600 text-xl mt-6 mb-4">Éditeur du site</h2>
+      <p class="text-gray-600 mb-4">
+        Nom de la Société : InformaCart<br>
+        Forme juridique : Société par Actions Simplifiée (SAS)<br>
+        Capital social : 100 000 €<br>
+        Adresse : 123 Rue de la Technologie, 75001 Paris<br>
+        Téléphone : 01 23 45 67 89<br>
+        Email : contact@informacart.com<br>
+        Numéro SIRET : 123 456 789 00010<br>
+        Numéro TVA intracommunautaire : FR 12 345678900
+      </p>
+      <h2 class="text-indigo-600 text-xl mt-6 mb-4">Directeur de la publication</h2>
+      <p class="text-gray-600 mb-4">
+        Le Directeur de la publication est Monsieur Jean Dupont, en qualité de Président de la société InformaCart.
+      </p>
+      <h2 class="text-indigo-600 text-xl mt-6 mb-4">Hébergement</h2>
+      <p class="text-gray-600 mb-4">
+        Le site est hébergé par :<br>
+        Nom de l'hébergeur : HostingProvider<br>
+        Forme juridique : Société à Responsabilité Limitée (SARL)<br>
+        Adresse : 456 Avenue de l'Internet, 75002 Paris<br>
+        Téléphone : 01 98 76 54 32<br>
+        Email : support@hostingprovider.com
+      </p>
+      <h2 class="text-indigo-600 text-xl mt-6 mb-4">Propriété intellectuelle</h2>
+      <p class="text-gray-600 mb-4">
+        Tous les éléments graphiques, la structure et, plus généralement, le contenu du site InformaCart sont protégés
+        par le droit d'auteur, le droit des marques et le droit des dessins et modèles.
+      </p>
+      <h2 class="text-indigo-600 text-xl mt-6 mb-4">Données personnelles</h2>
+      <p class="text-gray-600 mb-4">
+        Conformément à la loi Informatique et Libertés du 6 janvier 1978, vous disposez d'un droit d'accès, de
+        rectification, de modification et de suppression des données qui vous concernent. Vous pouvez exercer ce droit
+        en envoyant un courrier à l'adresse suivante : InformaCart, 123 Rue de la Technologie, 75001 Paris.
+      </p>
+      <h2 class="text-indigo-600 text-xl mt-6 mb-4">Conditions d'utilisation</h2>
+      <p class="text-gray-600 mb-4">
+        L'utilisation du site InformaCart implique l'acceptation pleine et entière des conditions générales
+        d'utilisation décrites ci-après. Ces conditions d'utilisation sont susceptibles d'être modifiées ou complétées à
+        tout moment, les utilisateurs du site InformaCart sont donc invités à les consulter de manière régulière.
+      </p>
+    </div>
+  </main>
 </template>
 
 <script setup lang="ts">
--- front/src/views/PrivacyPolicy.vue
+++ front/src/views/PrivacyPolicy.vue
@@ -1,43 +1,54 @@
 <template>
-  <div class="privacy-policy bg-gray-100 rounded-lg max-w-3xl mx-auto p-5">
-    <h1 class="text-gray-900 text-2xl mb-4">Politique de Confidentialité</h1>
-    <p class="text-gray-600 mb-4">
-      Chez InformaCart, nous nous engageons à protéger la confidentialité de nos utilisateurs.
-      Cette politique de confidentialité explique comment nous collectons, utilisons, divulguons
-      et protégeons vos informations.
-    </p>
-    <h2 class="text-indigo-600 text-xl mt-6 mb-4">Collecte des informations</h2>
-    <p class="text-gray-600 mb-4">
-      Nous collectons des informations lorsque vous vous inscrivez sur notre site, vous connectez à votre compte,
-      faites un achat, participez à un concours, et/ou lorsque vous vous déconnectez. Les informations collectées
-      incluent votre nom, votre adresse e-mail, votre numéro de téléphone et/ou votre carte de crédit.
-    </p>
-    <p class="text-gray-600 mb-4">
-      Nous collectons également des informations automatiquement via l'utilisation de cookies et autres technologies de suivi.
-    </p>
-    <h2 class="text-indigo-600 text-xl mt-6 mb-4">Utilisation des informations</h2>
-    <p class="text-gray-600 mb-4">
-      Toutes les informations que nous recueillons auprès de vous peuvent être utilisées pour :
-      - Personnaliser votre expérience et répondre à vos besoins individuels.
-      - Fournir un contenu publicitaire personnalisé.
-      - Améliorer notre site Web.
-      - Améliorer le service client et vos besoins de prise en charge.
-      - Vous contacter par e-mail.
-      - Administrer un concours, une promotion, ou une enquête.
-    </p>
-    <h2 class="text-indigo-600 text-xl mt-6 mb-4">Divulgation à des tiers</h2>
-    <p class="text-gray-600 mb-4">
-      Nous ne vendons, n'échangeons et ne transférons pas vos informations personnelles identifiables à des tiers. Cela ne comprend pas les tierce parties de confiance qui nous aident à exploiter notre site Web ou à mener nos affaires, tant que ces parties conviennent de garder ces informations confidentielles. Nous utilisons Stripe et PayPal pour le traitement des paiements. Ces tiers ont leurs propres politiques de confidentialité concernant les informations que nous sommes tenus de leur fournir pour les transactions liées à vos achats.
-    </p>
-    <h2 class="text-indigo-600 text-xl mt-6 mb-4">Protection des informations</h2>
-    <p class="text-gray-600 mb-4">
-      Nous mettons en œuvre une variété de mesures de sécurité pour préserver la sécurité de vos informations personnelles. Nous utilisons le cryptage pour protéger les informations sensibles transmises en ligne. Nous protégeons également vos informations hors ligne. Seuls les employés qui ont besoin d'effectuer un travail spécifique (par exemple, la facturation ou le service client) ont accès aux informations personnelles identifiables.
-    </p>
-    <h2 class="text-indigo-600 text-xl mt-6 mb-4">Consentement</h2>
-    <p class="text-gray-600 mb-4">
-      En utilisant notre site, vous consentez à notre politique de confidentialité.
-    </p>
-  </div>
+  <main>
+    <div class="privacy-policy bg-gray-100 rounded-lg max-w-3xl mx-auto p-5">
+      <h1 class="text-gray-900 text-2xl mb-4">Politique de Confidentialité</h1>
+      <p class="text-gray-600 mb-4">
+        Chez InformaCart, nous nous engageons à protéger la confidentialité de nos utilisateurs.
+        Cette politique de confidentialité explique comment nous collectons, utilisons, divulguons
+        et protégeons vos informations.
+      </p>
+      <h2 class="text-indigo-600 text-xl mt-6 mb-4">Collecte des informations</h2>
+      <p class="text-gray-600 mb-4">
+        Nous collectons des informations lorsque vous vous inscrivez sur notre site, vous connectez à votre compte,
+        faites un achat, participez à un concours, et/ou lorsque vous vous déconnectez. Les informations collectées
+        incluent votre nom, votre adresse e-mail, votre numéro de téléphone et/ou votre carte de crédit.
+      </p>
+      <p class="text-gray-600 mb-4">
+        Nous collectons également des informations automatiquement via l'utilisation de cookies et autres technologies
+        de suivi.
+      </p>
+      <h2 class="text-indigo-600 text-xl mt-6 mb-4">Utilisation des informations</h2>
+      <p class="text-gray-600 mb-4">
+        Toutes les informations que nous recueillons auprès de vous peuvent être utilisées pour :
+        - Personnaliser votre expérience et répondre à vos besoins individuels.
+        - Fournir un contenu publicitaire personnalisé.
+        - Améliorer notre site Web.
+        - Améliorer le service client et vos besoins de prise en charge.
+        - Vous contacter par e-mail.
+        - Administrer un concours, une promotion, ou une enquête.
+      </p>
+      <h2 class="text-indigo-600 text-xl mt-6 mb-4">Divulgation à des tiers</h2>
+      <p class="text-gray-600 mb-4">
+        Nous ne vendons, n'échangeons et ne transférons pas vos informations personnelles identifiables à des tiers.
+        Cela ne comprend pas les tierce parties de confiance qui nous aident à exploiter notre site Web ou à mener nos
+        affaires, tant que ces parties conviennent de garder ces informations confidentielles. Nous utilisons Stripe et
+        PayPal pour le traitement des paiements. Ces tiers ont leurs propres politiques de confidentialité concernant
+        les informations que nous sommes tenus de leur fournir pour les transactions liées à vos achats.
+      </p>
+      <h2 class="text-indigo-600 text-xl mt-6 mb-4">Protection des informations</h2>
+      <p class="text-gray-600 mb-4">
+        Nous mettons en œuvre une variété de mesures de sécurité pour préserver la sécurité de vos informations
+        personnelles. Nous utilisons le cryptage pour protéger les informations sensibles transmises en ligne. Nous
+        protégeons également vos informations hors ligne. Seuls les employés qui ont besoin d'effectuer un travail
+        spécifique (par exemple, la facturation ou le service client) ont accès aux informations personnelles
+        identifiables.
+      </p>
+      <h2 class="text-indigo-600 text-xl mt-6 mb-4">Consentement</h2>
+      <p class="text-gray-600 mb-4">
+        En utilisant notre site, vous consentez à notre politique de confidentialité.
+      </p>
+    </div>
+  </main>
 </template>
 
 <script setup lang="ts">
