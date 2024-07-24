722e760424b38e61ebb100c43a9167fbf4101f2d
Théo <davigny.theo@gmail.com>
2024-07-24 12:01:27 +0200

    secure orderProduct

--- back/src/routes/orderProduct.routes.js
+++ back/src/routes/orderProduct.routes.js
@@ -2,12 +2,15 @@
 const express = require('express');
 const router = express.Router();
 const orderProductController = require('../controllers/orderProduct.controller');
+const checkAuth = require('../middlewares/checkAuth');
+const checkRole = require('../middlewares/checkRole');
 
 // Routes pour les OrderProduct
 router.get('/', orderProductController.getAllOrderProducts);
-router.post('/', orderProductController.createOrderProduct);
-router.get('/:id', orderProductController.getOrderProductById);
-router.put('/:id', orderProductController.updateOrderProduct);
-router.delete('/:id', orderProductController.deleteOrderProduct);
+router.post('/', checkAuth, orderProductController.createOrderProduct);
+router.get('/:id', checkAuth, orderProductController.getOrderProductById);
+router.put('/:id', checkAuth, checkRole({ roles: ['ROLE_ADMIN'] }), orderProductController.updateOrderProduct);
+router.delete('/:id', checkAuth, checkRole({ roles: ['ROLE_ADMIN'] }), orderProductController.deleteOrderProduct);
 
 module.exports = router;
+
